7149cc32567e482a07fa266969dbda7e05dc5138
Filip Hrbek <filip.hrbek@gmail.com>
2017-09-20 12:05:45 +0200

    Use ES6 spread operator to break jscramble again

--- app/serializers/dude.js
+++ app/serializers/dude.js
@@ -3,7 +3,13 @@ import DS from 'ember-data';
 
 export default DS.RESTSerializer.extend({
   normalizeResponse: function (store, primaryModelClass, payload, id, requestType) {
-    return this._super(store, primaryModelClass, payload, id, requestType);
+
+    //this would work normally with jscramble
+    //return this._super(store, primaryModelClass, payload, id, requestType);
+
+    //this works fine normally, but with jscramble using the ES6 spread operator
+    //causes an error
+    return this._super(...arguments);
   }
 });
 
